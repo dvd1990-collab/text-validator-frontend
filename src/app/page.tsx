4f034aa88189f14eaece2a9fb316bc034d84f954
Davide Campagnolo <dvd.campagnolo@gmail.com>
2025-10-09 14:55:14 +0200

    Release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,8 @@ export default function HomePage() {
     setOutputText('Elaborazione in corso...');
     setQualityReport(null);
     try {
-      const response = await fetch('http://127.0.0.1:8000/validate', {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+	  const response = await fetch(`${apiUrl}/validate`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: inputText }),
